7745267b67027838199312766dd388660be60816
Amit Manchanda <amitdelhi1995@gmail.com>
2017-02-25 12:06:40 +0530

    First Commit

---
+++ Utils/extract_patches.py
@@ -0,0 +1,107 @@
+import random
+
+import numpy as np
+
+
+# Load the original img and return the extracted patches
+def get_img_training(img,
+                     groundTruth,
+                     patch_height,
+                     patch_width,
+                     N_subimgs,
+                     inside_FOV):
+    # train_imgs_original = load_hdf5(DRIVE_train_imgs_original)
+    # train_masks = load_hdf5(DRIVE_train_groudTruth)  # masks always the same
+    # # visualize(group_images(train_imgs_original[0:20,:,:,:],5),'imgs_train')#.show()  #check original imgs train
+
+
+    # train_imgs = my_PreProc(train_imgs_original)
+
+
+    # Converting white pixels to 1 and black to 0
+    groundTruth /= 255.
+
+    img = img[:, 9:574, :]  # cut bottom and top so now it is 565*565
+    groundTruth = groundTruth[:, :, 9:574, :]  # cut bottom and top so now it is 565*565
+
+    data_consistency_check(img, groundTruth)
+
+    # check masks are within 0-1
+    assert (np.min(groundTruth) == 0 and np.max(groundTruth) == 1)
+
+    print "\nimage/mask shape:"
+    print img.shape
+    print "image range (min-max): " + str(np.min(img)) + ' - ' + str(np.max(img))
+    print "Ground Truth are within 0-1\n"
+
+    # extract the TRAINING patches from the full images
+    patches_img, patches_groundTruth = extract_random(train_imgs, train_masks, patch_height, patch_width,
+                                                      N_subimgs, inside_FOV)
+    data_consistency_check(patches_imgs_train, patches_masks_train)
+
+    print "\ntrain PATCHES images/masks shape:"
+    print patches_imgs_train.shape
+    print "train PATCHES images range (min-max): " + str(np.min(patches_imgs_train)) + ' - ' + str(
+        np.max(patches_imgs_train))
+
+    return patches_imgs_train, patches_masks_train  # , patches_imgs_test, patches_masks_test
+
+
+# extract patches randomly in the full image
+#  -- Inside OR in full image
+def extract_random(full_img, full_groundTruth, patch_h, patch_w, N_patches, inside=True):
+    assert (len(full_img.shape) == 3 and len(full_groundTruth.shape) == 3)  # 3D arrays
+    assert (full_img.shape[0] == 1 or full_img.shape[0] == 3)  # check the channel is 1 or 3
+    assert (full_groundTruth.shape[0] == 1)  # masks only black and white
+    assert (full_img.shape[1] == full_groundTruth.shape[1] and full_img.shape[2] == full_groundTruth.shape[2])
+
+    patches = np.empty((N_patches, full_img.shape[0], patch_h, patch_w))
+    patches_groundTruth = np.empty((N_patches, full_groundTruth.shape[0], patch_h, patch_w))
+
+    img_h = full_img.shape[1]  # height of the full image
+    img_w = full_img.shape[2]  # width of the full image
+
+    # (0,0) in the center of the image
+
+    iter_tot = 0  # iter over the total number of patches (N_patches)
+    k = 0
+    while k < N_patches:
+        x_center = random.randint(0 + int(patch_w / 2), img_w - int(patch_w / 2))
+        # print "x_center " +str(x_center)
+        y_center = random.randint(0 + int(patch_h / 2), img_h - int(patch_h / 2))
+        # print "y_center " +str(y_center)
+        # check whether the patch is fully contained in the FOV
+        if inside:
+            if is_patch_inside_FOV(x_center, y_center, img_w, img_h, patch_h) == False:
+                continue
+        patch = full_imgs[i, :, y_center - int(patch_h / 2):y_center + int(patch_h / 2),
+                x_center - int(patch_w / 2):x_center + int(patch_w / 2)]
+        patch_mask = full_masks[i, :, y_center - int(patch_h / 2):y_center + int(patch_h / 2),
+                     x_center - int(patch_w / 2):x_center + int(patch_w / 2)]
+        patches[iter_tot] = patch
+        patches_masks[iter_tot] = patch_mask
+        iter_tot += 1  # total
+        k += 1  # per full_img
+    return patches, patches_masks
+
+
+def data_consistency_check(img, mask):
+    assert (len(img.shape) == len(mask.shape))
+    assert (img.shape[1] == mask.shape[1])
+    assert (img.shape[2] == mask.shape[2])
+    assert (mask.shape[0] == 1)
+    assert (img.shape[0] == 1 or img.shape[0] == 3)
+
+
+# check if the patch is fully contained in the FOV
+def is_patch_inside_FOV(x, y, img_w, img_h, patch_h):
+    x_ = x - int(img_w / 2)  # origin (0,0) shifted to image center
+    y_ = y - int(img_h / 2)  # origin (0,0) shifted to image center
+    R_inside = 270 - int(
+        patch_h * 1.42)  # radius is 270 (from DRIVE db docs), minus the patch diagonal (assumed it is a square this
+    # is the limit to contain the full patch in the FOV
+    radius = np.sqrt((x_ * x_) + (y_ * y_))
+    if radius < R_inside:
+        return True
+    else:
+        return False
---
+++ prepare_datasets_DRIVE.py
@@ -0,0 +1,65 @@
+from PIL import Image
+
+import h5py
+import numpy as np
+import os
+
+
+def write_hdf5(arr, outfile, name):
+    with h5py.File(outfile, "w") as f:
+        f.create_dataset(name, data=arr, dtype=arr.dtype)
+
+
+# ------------Path of the images --------------------------------------------------------------
+# train
+original_imgs_train = "./DRIVE/training/images/"
+groundTruth_imgs_train = "./DRIVE/training/1st_manual/"
+borderMasks_imgs_train = "./DRIVE/training/mask/"
+# test
+original_imgs_test = "./DRIVE/test/images/"
+groundTruth_imgs_test = "./DRIVE/test/1st_manual/"
+borderMasks_imgs_test = "./DRIVE/test/mask/"
+# ---------------------------------------------------------------------------------------------
+
+
+Nimgs = 20
+channels = 3
+height = 584
+width = 565
+dataset_path = "./DRIVE_datasets_training_testing/"
+
+
+def generate_datasets(imgs_dir, groundTruth_dir, borderMasks_dir, train_test="null"):
+    img = np.empty((height, width, channels))
+    groundTruth = np.empty((height, width))
+    border_mask = np.empty((height, width))
+    for path, subdirs, files in os.walk(imgs_dir):  # list all files, directories in the path
+        for i in range(len(files)):
+
+            # original
+            print "original image: " + files[i]
+            img = Image.open(imgs_dir + files[i])
+            img = np.asarray(img)
+
+            # corresponding ground truth
+            groundTruth_name = files[i][0:2] + "_manual1.gif"
+            print "ground truth name: " + groundTruth_name
+            groundTruth = Image.open(groundTruth_dir + groundTruth_name)
+            groundTruth = np.asarray(groundTruth)
+
+            # corresponding border masks
+            border_masks_name = ""
+            if train_test == "train":
+                border_masks_name = files[i][0:2] + "_training_mask.gif"
+            elif train_test == "test":
+                border_masks_name = files[i][0:2] + "_test_mask.gif"
+            else:
+                print "specify if train or test!!"
+                exit()
+            print "border masks name: " + border_masks_name
+            border_mask = Image.open(borderMasks_dir + border_masks_name)
+            border_mask = np.asarray(border_mask)
+
+
+generate_datasets(original_imgs_train, groundTruth_imgs_train,
+                  borderMasks_imgs_train, "train")
