b308cdf6dadee3049e9bd53649b9d7e0188784e2
Amit Manchanda <amitdelhi1995@gmail.com>
2017-02-27 14:18:26 +0530

    Reading the generated data

--- src/training.py
+++ src/training.py
@@ -1,16 +1,28 @@
 import ConfigParser
 
+import h5py
 from keras.models import Model
 from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D, Reshape, core, Dropout
 import numpy as np
+import sys
 
-from Utils.help_functions import load_hdf5
+
+# TODO: Use function in helpfunction
+# sys.path.append('../Utils')
+# from help_functions import load_hdf5
+# from Utils.help_functions import *
+
+
+def load_hdf5(infile):
+    with h5py.File(infile, "r") as f:  # "with" close the file after its nested commands
+        return f["img"][()], f["groundTruth"][()]
+
 
 # Load config params
 config = ConfigParser.RawConfigParser()
-config.read('configuration.txt')
+config.read('../configuration.txt')
 
-dataset_path = config.get('data paths', 'path_local')
+dataset_path = str(config.get('data paths', 'path_local'))
 
 
 # Define the neural network
@@ -54,8 +66,8 @@ def get_unet(n_ch, patch_height, patch_width):
     return model
 
 
-def trainmodel_epoch(train_sequence, val_sequence, model):
-    return final_training_accuracy, final_validation_accuracy
+# def trainmodel_epoch(train_sequence, val_sequence, model):
+#     return final_training_accuracy, final_validation_accuracy
 
 
 model = get_unet(n_ch=int(config.get('data attributes', 'channels')),
@@ -63,9 +75,31 @@ model = get_unet(n_ch=int(config.get('data attributes', 'channels')),
                  patch_height=int(config.get('data attributes', 'patch_height')))
 
 for i in range(5):
-    input_sequence = np.random.shuffle(np.arange(21, 39))
+    input_sequence = np.arange(21, 39)
+    np.random.shuffle(input_sequence)
+    print input_sequence
+    j = 0
     while j < len(input_sequence):
-        temp1 = load_hdf5(dataset_path + "training_patches_" + input_sequence[j])
-        temp2 = load_hdf5(dataset_path + "training_patches_" + input_sequence[j + 1])
-        temp3 = load_hdf5(dataset_path + "training_patches_" + input_sequence[j + 2])
-        print temp1.shape
+        print ("." + dataset_path)
+        temp_path1 = "." + dataset_path + "training_patches_" + str(input_sequence[j])
+        temp_path2 = "." + dataset_path + "training_patches_" + str(input_sequence[j + 1])
+        temp_path3 = "." + dataset_path + "training_patches_" + str(input_sequence[j + 2])
+        temp_img1, temp_gt1 = load_hdf5(temp_path1)
+        temp_img2, temp_gt2 = load_hdf5(temp_path2)
+        temp_img3, temp_gt3 = load_hdf5(temp_path3)
+        j = j + 3
+        X_train = np.append(temp_img1, temp_img2, axis=0)
+        y_train = np.append(temp_gt1, temp_gt2, axis=0)
+
+        del temp_img1
+        del temp_gt1
+        del temp_img2
+        del temp_gt2
+
+        X_train = np.append(X_train,temp_img3,axis=0)
+        y_train = np.append(y_train,temp_gt3,axis=0)
+
+        del temp_img3
+        del temp_gt3
+
+        print X_train.shape, y_train.shape
